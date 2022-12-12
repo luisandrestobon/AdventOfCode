073cca945acb74416cd9e60737121d427d792941
Luis Tobon <luis.tobon@geniussports.com>
2022-12-12 14:55:29 -0500

    Resolve year 2021 day 06 part 1.

--- src/main/java/year2021/day06/part1/Lanternfish.java
+++ src/main/java/year2021/day06/part1/Lanternfish.java
@@ -3,38 +3,23 @@ package year2021.day06.part1;
 import java.util.Arrays;
 
 public class Lanternfish {
-    public long pointsOverlaping(String input) {
-        int[] values = Arrays.stream(input.split(",")).mapToInt(Integer::parseInt).toArray();
-        long[] counts = new long[9];
-        printLongArray(counts);
-        initialize(counts, values);
-        printLongArray(counts);
-        simulate(counts, 256);
-        return Arrays.stream(counts).sum();
+
+    private static final int MAX_DIGIT = 8;
+    private static final int MAX_DAYS = 80;
+
+    public long pointsOverlaping(String testStr) {
+        int[] counters = new int[MAX_DIGIT + 1];
+        Arrays.stream(testStr.split(",")).mapToInt(Integer::parseInt).forEach(i -> counters[i]++);
+        for (int i = 0; i < MAX_DAYS; i++) {
+            simulateDay(counters);
+        }
+        return Arrays.stream(counters).sum();
     }
 
-    private void initialize(long[] counts, int[] values) {
-        for (int i = 0; i < values.length; i++) {
-            counts[values[i]]++;
-        }
-    }
-
-    private void simulate(long[] counts, int days) {
-        for (int i = 0; i < days; i++) {
-            long zero = counts[0];
-            for (int j = 1; j < 9; j++) {
-                counts[j - 1] = counts[j];
-            }
-            counts[6] += zero;
-            counts[8] = zero;
-            //printLongArray(counts);
-        }
-    }
-
-    public void printLongArray(long[] array) {
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i] + " ");
-        }
-        System.out.println("");
+    private void simulateDay(int[] counters) {
+        int zeroValue = counters[0];
+        System.arraycopy(counters, 1, counters, 0, MAX_DIGIT);
+        counters[MAX_DIGIT - 2] = counters[MAX_DIGIT - 2] + zeroValue;
+        counters[MAX_DIGIT] = zeroValue;
     }
 }
--- src/test/java/year2021/day06/part1/LanternfishTest.java
+++ src/test/java/year2021/day06/part1/LanternfishTest.java
@@ -1,19 +1,25 @@
-package year2021.day06.par1;
+package year2021.day06.part1;
 
 import org.junit.jupiter.api.Test;
-import year2021.day06.part1.Lanternfish;
 
 import static common.FileModule.file2String;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LanternfishTest {
-
-    private Lanternfish lanternfish = new Lanternfish();
+    private final Lanternfish lanternfish = new Lanternfish();
 
     @Test
     void firstTest() {
         String testStr = file2String("src/test/resources/year2021/test_day06.txt");
         long expected = 5934L;
+        long actual = lanternfish.pointsOverlaping(testStr);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void inputTest() {
+        String testStr = file2String("src/test/resources/year2021/input_day06.txt");
+        long expected = 379414L;
         long actual = lanternfish.pointsOverlaping(testStr);
         assertEquals(expected, actual);
     }
